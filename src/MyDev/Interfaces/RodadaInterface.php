e2b13dba43d46d2f706a805ac3cd2c8dc6ae7795
Saulo <saulo@saulovieira.com.br>
2014-12-07 11:20:26 -0200

    Atualização de cometários

--- src/MyDev/Interfaces/RodadaInterface.php
+++ src/MyDev/Interfaces/RodadaInterface.php
@@ -2,21 +2,53 @@
 
 namespace MyDev\Interfaces;
 
+/**
+ * Interface RodadaInterface
+ * @package MyDev\Interfaces
+ */
 interface RodadaInterface {
 
+    /**
+     * @param PartidaInterface $partida
+     */
     public function __construct(PartidaInterface $partida);
 
+    /**
+     * @param JogadorInterface $jogador
+     * @param $valor
+     * @return mixed
+     */
     public function apostar(JogadorInterface $jogador, $valor);
 
+    /**
+     * @param JogadorInterface $jogador
+     * @return mixed
+     */
     public function mesa(JogadorInterface $jogador);
 
+    /**
+     * @param JogadorInterface $jogador
+     * @return void
+     */
     public function correr(JogadorInterface $jogador);
 
+    /**
+     * @return void
+     */
     public function preFlopy();
 
+    /**
+     * @return void
+     */
     public function flopy();
 
+    /**
+     * @return void
+     */
     public function turn();
 
+    /**
+     * @return void
+     */
     public function river();
 }
